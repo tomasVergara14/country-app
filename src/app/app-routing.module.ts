fc75c15fc44674bf4a802755acf131bb529d4081
Tom <tomasvergara272@gmail.com>
2023-06-17 05:06:30 -0300

    routing module

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AboutPageComponent } from './shared/pages/about-page/about-page.component';
+import { HomePageComponent } from './shared/pages/home-page/home-page.component';
+
+const routes: Routes = [
+  {
+    path:'home',
+    component: HomePageComponent
+  },
+  {
+    path:'about',
+    component: AboutPageComponent
+  },
+  {
+    path:'**',
+    redirectTo: 'home'
+  },
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot( routes ),
+  ],
+  exports:[
+    RouterModule,
+  ]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    NgbModule
+    NgbModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
