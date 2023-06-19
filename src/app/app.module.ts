4efa41fe20c880fc91d9fcff10092287787df2d3
Tom <tomasvergara272@gmail.com>
2023-06-19 01:15:53 -0300

    countries pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,10 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { SharedModule } from './shared/shared.module';
-import { ContactPagesComponent } from './ctshared/pages/contact-pages/contact-pages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContactPagesComponent
   ],
   imports: [
     AppRoutingModule,
---
+++ src/app/countries/countries-routing.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { Routes } from '@angular/router';
+
+const routes: Routes = []
+
+@NgModule({
+  declarations: [],
+  imports: [],
+  exports:[]
+})
+export class CountriesRoutingModule { }
