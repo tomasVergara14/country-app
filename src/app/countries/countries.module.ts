3ddf234b9872b6abc26f7cc4a654d6cbea4bbd0a
Tom <tomasvergara272@gmail.com>
2023-06-20 03:36:00 -0300

    countries table

--- src/app/countries/countries.module.ts
+++ src/app/countries/countries.module.ts
@@ -8,6 +8,7 @@ import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.
 import { ByCountryPageComponent } from './pages/by-country-page/by-country-page.component';
 import { ByRegionPageComponent } from './pages/by-region-page/by-region-page.component';
 import { CountryPageComponent } from './pages/country-page/country-page.component';
+import { CountryTableComponent } from './components/country-table/country-table.component';
 
 
 @NgModule({
@@ -15,7 +16,8 @@ import { CountryPageComponent } from './pages/country-page/country-page.componen
     ByCapitalPageComponent,
     ByCountryPageComponent,
     ByRegionPageComponent,
-    CountryPageComponent
+    CountryPageComponent,
+    CountryTableComponent
   ],
   imports: [
     CountriesRoutingModule,
