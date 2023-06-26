15acc835766686a07e11b9666fdb03dfbbf8bbdd
Tom <tomasvergara272@gmail.com>
2023-06-26 00:46:23 -0300

    activated route

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CountriesService } from '../../services/countries.service';
 
 @Component({
   selector: 'countries-country-page',
   templateUrl: './country-page.component.html',
   styleUrls: ['./country-page.component.scss']
 })
-export class CountryPageComponent {
+export class CountryPageComponent implements OnInit {
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private countriesService: CountriesService,
+  ){}
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe( ({ id })=>{                                       // Reacciona a cambios en los parametros de la ruta
+      
+      console.log( { params: id } )
+      this.countriesService.onSearchRestCountries('alpha', id).subscribe( alphaCountry => { //Un subscribe dentro de otro
+        console.log(alphaCountry)
+      })
+
+    })
+  }
 
 }
