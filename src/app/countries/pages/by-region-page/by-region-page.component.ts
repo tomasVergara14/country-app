6155df01026cc10f773118ee2ed1559fa4e38bae
Tom <tomasvergara272@gmail.com>
2023-07-01 21:23:00 -0300

    byRegion

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,11 +2,16 @@ import { Component } from '@angular/core';
 import { CountriesService } from '../../services/countries.service';
 import { Country } from '../../interfaces/country';
 
+
+type Region = 'Africa' | 'Americas' | 'Asia' | 'Europe' | 'Oceania'
+
 @Component({
   selector: 'countries-by-region-page',
   templateUrl: './by-region-page.component.html',
   styleUrls: ['./by-region-page.component.scss']
 })
+
+
 export class ByRegionPageComponent {
 
   constructor(
@@ -14,9 +19,14 @@ export class ByRegionPageComponent {
   ){}
 
   public countries:Country[] = [];
+  public regions: Region[] = ['Africa','Americas', 'Asia', 'Europe', 'Oceania'];
+  public selectedRegion ?: Region;
 
-  public onSearchByRegion( term: string ):void{
-    this.countriesService.onSearchRestCountries('region',term).subscribe( countries=>{
+  public onSearchByRegion( region: Region ):void{
+
+    this.selectedRegion = region;
+
+    this.countriesService.onSearchRestCountries('region',region).subscribe( countries=>{
       this.countries = countries;
     })
     console.log(this.countries)
